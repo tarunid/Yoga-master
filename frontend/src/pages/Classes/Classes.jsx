fe9e2db67a5e52490a8e9e1eb2bb9492c044e776
taruni d <tarunid.2003@gmail.com>
2024-05-21 13:32:25 +0530

    updated file

--- frontend/src/pages/Classes/Classes.jsx
+++ frontend/src/pages/Classes/Classes.jsx
@@ -1,44 +1,65 @@
-import React , {useState, useEffect}from 'react'
-import useAxiosFetch from '../../hooks/useAxiosFetch';
+import React, { useState, useEffect } from "react";
+import useAxiosFetch from "../../hooks/useAxiosFetch";
+import { Transition } from "@headlessui/react";
 
 export const Classes = () => {
   const [classes, setClasses] = useState([]);
-  const[hoveredCard, setHoveredCard] =useState(null);
+  const [hoveredCard, setHoveredCard] = useState(null);
   const axiosFetch = useAxiosFetch();
 
   const handleHover = (index) => {
     setHoveredCard(index);
-  }
+  };
   useEffect(() => {
-    axiosFetch.get('/classes').then(res => setClasses(res.data)).catch(err => console.log(err))
-
-  },[]);
-  console.log(classes)
+    axiosFetch
+      .get("/classes")
+      .then((res) => setClasses(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+  console.log(classes);
   return (
     <div>
-      <div className='mt-20 pt-3'>
-        <h1 className='text-4xl font-bold text-center text-primary'></h1>
+      <div className="mt-20 pt-3">
+        <h1 className="text-4xl font-bold text-center text-primary">Classes</h1>
       </div>
 
-      <div>
-        {
-          classes.map((cls, index) => (
-            <div
-            // key={index}
-            // className={`relative hover:-translate-y-2 duration-150 hover:ring-[2px] hover:ring-secondary w-64
-            // h-[350px] mx-auto ${cls.availableSeats <1? 'bg-red-300': 'bg-white'} dark:bg-slate-600
-            // rounded-lg shadow-lg overflow-hidden cursor-pointer`}
-            >
-              <div className='relative h-48'>
-                <div className={`absolute inset-0 bg-black opacity-0 transition-opacity duration-300 
-                ${hoveredCard === index ? "opacity-60 ": ""}`}/>
-                <img src={cls.image} alt=""  className='object-cover w-full h-full'/>
-
-              </div>
+      <div className="my-16 w-[90%] mx-auto grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8">
+        {classes.slice(0, 6).map((cls, index) => (
+          <div
+            onMouseLeave={() => handleHover(null)}
+            key={index}
+            className={`relative hover:-translate-y-2 duration-150 hover:ring-[2px] hover:ring-secondary w-64
+             h-[350px] mx-auto ${
+               cls.availableSeats < 1 ? "bg-red-300" : "bg-white"
+             } dark:bg-slate-600
+             rounded-lg shadow-lg overflow-hidden cursor-pointer`}
+            onMouseEnter={() => handleHover(index)}
+          >
+            <div className="relative h-48">
+              <div
+                className={`absolute inset-0 bg-black opacity-0 transition-opacity duration-300 
+                ${hoveredCard === index ? "opacity-60 " : ""}`}
+              />
+              <img
+                src={cls.image}
+                alt=""
+                className="object-cover w-full h-full"
+              />
+              <Transition
+                show={hoveredCard === index}
+                enter="transition-opacity duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="transition-opacity duration-300"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <div>I will fade in and out</div>
+              </Transition>
             </div>
-          ))
-        }
+          </div>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
